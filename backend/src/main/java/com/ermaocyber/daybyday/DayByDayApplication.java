3716cbb13a522c39453f9ca2808b8bf2e69d520a
Jason Yang <jason.yang.sd@gmail.com>
2026-09-19 14:09:51 +1200

    Create minimal Spring Boot backend skeleton

---
+++ backend/src/main/java/com/ermaocyber/daybyday/DayByDayApplication.java
@@ -0,0 +1,12 @@
+package com.ermaocyber.daybyday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayByDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayByDayApplication.class, args);
+    }
+}
