20aad9238630e3511e84b0d9b0a3dbf56e3bf19e
Jason Yang <jason.yang.sd@gmail.com>
2026-09-19 22:05:06 +1200

    Generate Card UUIDs with JPA

--- backend/src/main/java/com/ermaocyber/daybyday/card/domain/Card.java
+++ backend/src/main/java/com/ermaocyber/daybyday/card/domain/Card.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -15,6 +17,7 @@ import java.util.UUID;
 public class Card {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(name = "user_id", nullable = false)
